296927b8915e5fd81edbb8c292ec8a721682fea8
Brian Beck <brian@lightshipworks.com>
2020-12-12 01:12:42 -0800

    Add class icons and format date

--- ggat-app/src/app/services/arena-match.service.ts
+++ ggat-app/src/app/services/arena-match.service.ts
@@ -11,27 +11,27 @@ export class ArenaMatchService {
    getMatches(){
     let matchHistory = [
       {
-        date: 1607766603,
-        team:["Balance Druid", "Shadow Priest"],
-        enemyTeam:["Subtlety Rogue", "Fury Warrior"],
+        date: 1607763784000,
+        team:[{name: "Balance Druid", class: "druid"}, {name: "Shadow Priest", class: "priest"}],
+        enemyTeam:[{name: "Subtlety Rogue", class: "rogue"}, {name: "Fury Warrior", class: "warrior"}],
         rating: 1245,
         enemyRating: 1125,
         win: true,
         ratingChange: 24
       },
       {
-        date: 16077666998,
-        team:["Balance Druid", "Shadow Priest"],
-        enemyTeam:["Restoration Shaman", "Retribution Paladin"],
+        date: 1607764785000,
+        team:[{name: "Balance Druid", class: "druid"}, {name: "Shadow Priest", class: "priest"}],
+        enemyTeam:[{name: "Restoration Shaman", class: "shaman"}, {name: "Retribution Paladin", class: "paladin"}],
         rating: 1270,
         enemyRating: 1347,
         win: false,
         ratingChange: 12
       },
       {
-        date: 16077678344,
-        team:["Balance Druid", "Shadow Priest"],
-        enemyTeam:["Frost Death Knight", "Windwalker Monk"],
+        date: 1608765786000,
+        team:[{name: "Balance Druid", class: "druid"}, {name: "Shadow Priest", class: "priest"}],
+        enemyTeam:[{name: "Frost Deathknight", class: "deathknight"}, {name: "Windwalker Monk", class: "monk"}],
         rating: 1258,
         enemyRating: 1355,
         win: true,
